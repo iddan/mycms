be5e97b6ac0bfa50f31cff0c6729e064f12afb01
iddan <iddan_gunawan@yahoo.co.id>
2015-10-23 10:47:25 +0700

    step1 and step 2

---
+++ application/core/Public_Controller.php
@@ -0,0 +1,11 @@
+<?php
+ 
+class Public_Controller extends MY_Controller {
+
+  	function __construct()
+  	{
+    	parent::__construct();
+    	echo 'This is from public controller';
+  	}
+  	
+}
---
+++ application/views/admin/dashboard_view.php
@@ -0,0 +1,4 @@
+<div id="container">
+  	<h1>Welcome to the Dashboard!</h1>
+  	<?php echo $this->config->item('cms_title');?>
+</div>
